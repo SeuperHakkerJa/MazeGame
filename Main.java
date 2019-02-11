7df036d39e94dab78485218972e5973b8b83806c
Xiaochen Li <jacobxiaochen@sina.com>
2019-02-11 01:04:06 -0800

    version 1.0 done

--- Main.java
+++ Main.java
@@ -1,11 +1,25 @@
 import objectdraw.*;
 
+import javax.swing.*;
+import java.awt.*;
 
-public class Main extends WindowController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Main extends WindowController implements ActionListener {
 
   public static int columns = 8, rows = 8;
   public static Maze maze;
   public static MazeSolver ms;
+
+  public static int columnInput, rowInput;
+
+  public JPanel buttonPanel, functionPanel;
+  public JButton nextStepButton, showPathButton, wallButton, startButton,
+    endButton, solveButton;
+
+  public boolean wallOpen, startOpen, endOpen;
 
 
 
@@ -19,36 +33,19 @@ public class Main extends WindowController {
   }
 
   public void begin() {
+
+    constructLayout();
     initGame();
-    solve();
+
   }
+
 
   public void initGame() {
     columns = 8;
     rows = 8;
     maze = new Maze(rows, columns, canvas);
     maze.draw(canvas);
-    maze.setWall(3, 2);
-    maze.setWall(4, 2);
-    maze.setWall(5, 2);
-    maze.setWall(6, 2);
 
-
-    maze.setWall(2, 4);
-    maze.setWall(2, 6);
-    maze.setWall(2, 5);
-    maze.setWall(6, 5);
-    maze.setWall(6, 2);
-    maze.setWall(6, 4);
-    maze.setWall(6, 3);
-    maze.setWall(7, 2);
-
-
-    maze.setWall(5, 6);
-    maze.setWall(0, 6);
-    maze.setWall(1, 6);
-    maze.setStart(0, 0);
-    maze.setEnd(6, 6);
     ms = new MazeSolver();
 
 
@@ -56,8 +53,23 @@ public class Main extends WindowController {
 
   public void onMouseClick(Location point) {
 
-    ms.showPath();
-    ms.getNext(maze);
+
+    System.out.println(point.getX() + " " + point.getY());
+    //
+    //ms.showPath();
+    //ms.getNext(maze);
+    int x = (int) (point.getX() / Constants.GRID_CELL_SIZE);
+    int y = (int) (point.getY() / Constants.GRID_CELL_SIZE);
+
+    if (wallOpen) {
+      maze.setWall(x, y);
+    } else if (startOpen) {
+      maze.setStart(x,y);
+      startButton.setEnabled(false);
+    } else if (endOpen) {
+      maze.setEnd(x,y);
+      endButton.setEnabled(false);
+    }
 
 
   }
@@ -65,12 +77,74 @@ public class Main extends WindowController {
 
 
 
-  public void solve(){
+  public void solve() {
     ms = new MazeSolver();
     ms.solve(maze, new QueueWorklist());
 
+  }
+
+  public void constructLayout() {
+
+    buttonPanel = new JPanel(new GridLayout(1, 3));
+    nextStepButton = new JButton("Next Step");
+    showPathButton = new JButton("Show Path");
+    solveButton = new JButton("Solve");
+    buttonPanel.add(solveButton);
+    buttonPanel.add(nextStepButton);
+    buttonPanel.add(showPathButton);
+    this.add(buttonPanel, BorderLayout.SOUTH);
+
+    functionPanel = new JPanel(new GridLayout(3, 1));
+    wallButton = new JButton("set Wall");
+    startButton = new JButton("set Start");
+    endButton = new JButton("set End");
+    functionPanel.add(wallButton);
+    functionPanel.add(startButton);
+    functionPanel.add(endButton);
+    this.add(functionPanel, BorderLayout.EAST);
+
+    wallButton.addActionListener(this);
+    startButton.addActionListener(this);
+    endButton.addActionListener(this);
+    solveButton.addActionListener(this);
+    nextStepButton.addActionListener(this);
+
+
+    this.validate();
 
   }
 
+  public void actionPerformed(ActionEvent evt) {
 
+    String actionCommand = evt.getActionCommand();
+
+    if (actionCommand.equals("set Wall")) {
+      wallOpen = !wallOpen;
+      startOpen = false;
+      endOpen = false;
+
+    } else if (actionCommand.equals("Solve")) {
+
+      solve();
+      wallButton.setEnabled(false);
+      startButton.setEnabled(false);
+      endButton.setEnabled(false);
+
+    } else if (actionCommand.equals("Next Step")) {
+
+      ms.showPath();
+      ms.getNext(maze);
+
+    } else if (actionCommand.equals("set Start")){
+
+      startOpen = !startOpen;
+      endOpen = false;
+      wallOpen = false;
+    } else if (actionCommand.equals("set End")){
+      endOpen = !endOpen;
+      startOpen = false;
+      wallOpen = false;
+    }
+
+  }
 }
